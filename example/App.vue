54df8ecfaeef46dd9d9ef6ace4771364340900c7
martsforever <martsforever@aliyun.com>
2019-09-13 23:45:18 +0800

    home page

--- example/App.vue
+++ example/App.vue
@@ -1,10 +1,38 @@
 <template>
     <div id="app">
-        <vue-codemirror-editor/>
+        <div class="app-head">
+            <div class="app-head-content">
+                Martsforever
+            </div>
+        </div>
+        <div class="app-body">
+            <div class="app-body-desc">
+                <div>
+                    <span class="app-body-desc-title">
+                        Vue-Codemirror-Editor
+                    </span>
+                    <span class="app-body-desc-label">
+                      Vue component base on CodeMirror
+                    </span>
+                </div>
+            </div>
+            <div class="app-body-tabs">
+                <a class="app-body-tab">Example</a>
+                <a class="app-body-tab" href="#">Install</a>
+                <a class="app-body-tab" href="https://codemirror.net/" target="_blank">CodeMirror</a>
+                <a class="app-body-tab" href="https://codemirror.net/demo/theme.html" target="_blank">Theme</a>
+                <a class="app-body-tab" href="https://codemirror.net/mode/index.html" target="_blank">Language</a>
+                <a class="app-body-tab" target="_blank">Github</a>
+            </div>
+        </div>
+        <div class="app-foot">
+
+        </div>
     </div>
 </template>
 
 <script>
+
     export default {
         name: 'app',
     }
@@ -16,5 +44,75 @@
         width: 100%;
         margin: 0;
         padding: 0;
+        overflow: auto;
+        font-family: sans-serif;
+
+        .app-head {
+            background-color: #12b4a5;
+            height: 64px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            .app-head-content {
+                width: 1000px;
+                color: white;
+                font-size: 27px;
+            }
+        }
+
+        .app-body {
+            .app-body-desc {
+                background-color: #f6f6f6;
+                padding: 40px 0;
+                letter-spacing: 1px;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+
+                & > div {
+                    width: fit-content;
+                    display: inline-flex;
+                    flex-direction: column;
+                    align-items: flex-end;
+                }
+
+                .app-body-desc-title {
+                    color: #333;
+                    font-size: 36px;
+                }
+
+                .app-body-desc-label {
+                    color: #999;
+                    height: 44px;
+                    line-height: 44px;
+                }
+            }
+
+            .app-body-tabs {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                padding: 9px 0;
+                cursor: pointer;
+                user-select: none;
+
+                .app-body-tab {
+                    padding: 9px 16px;
+                    text-decoration: none;
+                    color: #333;
+
+                    &:hover {
+                        border-radius: 3px;
+                        background-color: #f6f6f6;
+                    }
+
+                    &:active {
+                        background: #e2e2e2;
+                    }
+                }
+            }
+        }
     }
 </style>
