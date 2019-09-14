782294569016925348aa271b10fa1606f45662d3
martsforever <martsforever@aliyun.com>
2019-09-14 11:03:30 +0800

    adjust config

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ const path = require('path')
 const resolve = (dir) => path.join(__dirname, dir)
 
 module.exports = {
+    publicPath: '/vue-codemirror-editor/',
+    outputDir: 'page',
     pages: {
         index: {
             entry: 'example/main.js',
